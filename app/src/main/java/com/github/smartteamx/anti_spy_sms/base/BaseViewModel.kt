f12ed897c418c1768a3ff55eccf6919caef4214d
TurKurT656 <turkurt656@gmail.com>
2019-12-02 23:18:14 +0330

    added navigation command event to the base viewmodel to support navigation

--- app/src/main/java/com/github/smartteamx/anti_spy_sms/base/BaseViewModel.kt
+++ app/src/main/java/com/github/smartteamx/anti_spy_sms/base/BaseViewModel.kt
@@ -1,5 +1,12 @@
 package com.github.smartteamx.anti_spy_sms.base
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.smartteamx.anti_spy_sms.util.Event
+import com.github.smartteamx.anti_spy_sms.util.navigation.NavigationCommand
 
-abstract class BaseViewModel(private val model: BaseModel) : ViewModel()
+abstract class BaseViewModel(private val model: BaseModel) : ViewModel() {
+
+    val navigationCommand =  MutableLiveData<Event<NavigationCommand>>()
+
+}
