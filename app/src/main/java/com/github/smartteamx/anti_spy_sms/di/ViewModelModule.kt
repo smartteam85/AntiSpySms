015aa3a2be8f855e8b770cb93f81134935dee0bd
ahmad <ahmadghorbi74@gmail.com>
2019-12-17 02:07:30 +0330

    add view models to koin

--- app/src/main/java/com/github/smartteamx/anti_spy_sms/di/ViewModelModule.kt
+++ app/src/main/java/com/github/smartteamx/anti_spy_sms/di/ViewModelModule.kt
@@ -1,10 +1,15 @@
 package com.github.smartteamx.anti_spy_sms.di
 
+import com.github.smartteamx.anti_spy_sms.ui.conversations.ConversationsViewModel
+import com.github.smartteamx.anti_spy_sms.ui.main.MainViewModel
+import org.koin.android.ext.koin.androidApplication
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 /**
  * ViewModel's (VM in M V VM) module
  */
 val viewModelModule = module {
-
+    viewModel { MainViewModel(get()) }
+    viewModel { ConversationsViewModel(androidApplication(),get()) }
 }
