6af4051078a0d8974a60811fca17c293f9e5b635
ahmad <ahmadghorbi74@gmail.com>
2019-12-17 02:17:13 +0330

    add main model and conversation model to koin

--- app/src/main/java/com/github/smartteamx/anti_spy_sms/di/ModelModule.kt
+++ app/src/main/java/com/github/smartteamx/anti_spy_sms/di/ModelModule.kt
@@ -1,10 +1,13 @@
 package com.github.smartteamx.anti_spy_sms.di
 
+import com.github.smartteamx.anti_spy_sms.ui.conversations.ConversationsModel
+import com.github.smartteamx.anti_spy_sms.ui.main.MainModel
 import org.koin.dsl.module
 
 /**
  * Model's (M in M V VM) module
  */
 val modelModule = module {
-
+    factory { MainModel() }
+    factory { ConversationsModel(get()) }
 }
