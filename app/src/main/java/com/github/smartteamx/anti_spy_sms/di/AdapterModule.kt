78d12cd74fe580df0105aace14d1494186f8187a
ahmad <ahmadghorbi74@gmail.com>
2019-12-17 02:09:26 +0330

    add conversationAdapter to koin

--- app/src/main/java/com/github/smartteamx/anti_spy_sms/di/AdapterModule.kt
+++ app/src/main/java/com/github/smartteamx/anti_spy_sms/di/AdapterModule.kt
@@ -1,10 +1,11 @@
 package com.github.smartteamx.anti_spy_sms.di
 
+import com.github.smartteamx.anti_spy_sms.ui.conversations.ConversationsAdapter
 import org.koin.dsl.module
 
 /**
  * RecyclerView's adapters module
  */
 val adapterModule = module {
-
+    factory { ConversationsAdapter() }
 }
