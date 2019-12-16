5717a2916ee1f8ce35b48b6b43f1b613d30ed468
ahmad <ahmadghorbi74@gmail.com>
2019-12-17 02:18:19 +0330

    add conversation repository to koin

--- app/src/main/java/com/github/smartteamx/anti_spy_sms/di/RepositoryModule.kt
+++ app/src/main/java/com/github/smartteamx/anti_spy_sms/di/RepositoryModule.kt
@@ -1,10 +1,13 @@
 package com.github.smartteamx.anti_spy_sms.di
 
+import com.github.smartteamx.anti_spy_sms.data.repositories.ConversationsRepository
+import com.github.smartteamx.anti_spy_sms.data.repositories.ConversationsRepositoryImpl
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 /**
  * Repository's module
  */
 val repositoryModule = module {
-
+    factory <ConversationsRepository>{ ConversationsRepositoryImpl(androidApplication()) }
 }
